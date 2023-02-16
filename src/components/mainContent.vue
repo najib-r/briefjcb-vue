253c993894fca7b79c0c1ddd55693881fbb1bb2c
najib-r <najib72645@gmail.com>
2023-02-16 13:47:11 +0800

    Use v-data-table

--- src/components/mainContent.vue
+++ src/components/mainContent.vue
@@ -1,40 +1,53 @@
 <template>
-    <v-simple-table dense fixed-header height="300px">
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th class="text-subtitle-1 font-weight-bold">
-              Job position
-            </th>
-            <th class="text-subtitle-1 font-weight-bold">
-              Company
-            </th>
-            <th class="text-subtitle-1 font-weight-bold">
-              Salary
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="job in jobs" :key="job.name">
-            <td><a :href="job.link" class="links" target="_blank" rel="noopener noreferrer">{{ job.name }}</a></td>
-            <td>{{ job.company }}</td>
-            <td>{{ job.salary }}</td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
-  </template>
+<v-card>
+  <v-card-title>
+    <v-text-field
+    v-model="search"
+    append-icon="mdi-magnify"
+    label="Search"
+    single-line
+    hide-details
+    ></v-text-field>
+  </v-card-title>
+  <v-data-table
+    :headers="headers"
+    :items="jobs"
+    :search="search"
+    disable-pagination
+    hide-default-footer
+    dense>
+  </v-data-table>
+</v-card>
+</template>
 
 <script>
 export default {
     name: 'mainContent',
     data () {
         return {
+            search: '',
             jobs: [
                 {name: 'job 1', company: 'company 1', salary: '$ salary', link: 'https://google.com'},
                 {name: 'job 2', company: 'company 2', salary: '$ salary', link: 'https://google.com'},
                 {name: 'job 3', company: 'company 3', salary: '$ salary', link: 'https://google.com'},
-                {name: 'job 4', company: 'company 4', salary: '$ salary', link: 'https://google.com'}
+                {name: 'job 4', company: 'company 4', salary: '$ salary', link: 'https://google.com'},
+                {name: 'job 5', company: 'company 5', salary: '$ salary', link: 'https://google.com'},
+                {name: 'job 6', company: 'company 6', salary: '$ salary', link: 'https://google.com'},
+                {name: 'job 7', company: 'company 7', salary: '$ salary', link: 'https://google.com'},
+                {name: 'job 8', company: 'company 8', salary: '$ salary', link: 'https://google.com'},
+                {name: 'job 9', company: 'company 9', salary: '$ salary', link: 'https://google.com'},
+                {name: 'job 10', company: 'company 10', salary: '$ salary', link: 'https://google.com'},
+                {name: 'job 11', company: 'company 11', salary: '$ salary', link: 'https://google.com'},
+            ],
+            headers: [
+                {
+                  text: 'Job position',
+                  align: 'start',
+                  sortable: true,
+                  value: 'name',
+                },
+                { text: 'Company', value: 'company' },
+                { text: 'Salary', value: 'salary' }
             ]
         }
     }
