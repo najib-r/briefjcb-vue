cea659dc604fe57d4446499f9c5d896001d87e3c
najib-r <najib72645@gmail.com>
2023-02-16 11:19:12 +0800

    Add links

--- src/components/headerContent.vue
+++ src/components/headerContent.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <p class="text-h5">briefJCB</p>
+        <p class="text-h5 green--text text--darken-3">briefJCB</p>
         <p class="text-body-1">A simple page showing the 75 most recent job vacancies (top one being most recent) on jobcentrebrunei. Click on a job position to view the job details page on JCB. </p>
     </div>
 </template>
--- src/components/mainContent.vue
+++ src/components/mainContent.vue
@@ -3,20 +3,20 @@
       <template v-slot:default>
         <thead>
           <tr>
-            <th>
+            <th class="text-subtitle-1 font-weight-bold">
               Job position
             </th>
-            <th>
+            <th class="text-subtitle-1 font-weight-bold">
               Company
             </th>
-            <th>
+            <th class="text-subtitle-1 font-weight-bold">
               Salary
             </th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="job in jobs" :key="job.name">
-            <td>{{ job.name }}</td>
+            <td><a :href="job.link" class="links" target="_blank" rel="noopener noreferrer">{{ job.name }}</a></td>
             <td>{{ job.company }}</td>
             <td>{{ job.salary }}</td>
           </tr>
@@ -31,12 +31,34 @@ export default {
     data () {
         return {
             jobs: [
-                {name: 'job 1', company: 'company 1', salary: '$ salary'},
-                {name: 'job 2', company: 'company 2', salary: '$ salary'},
-                {name: 'job 3', company: 'company 3', salary: '$ salary'},
-                {name: 'job 4', company: 'company 4', salary: '$ salary'}
+                {name: 'job 1', company: 'company 1', salary: '$ salary', link: 'https://google.com'},
+                {name: 'job 2', company: 'company 2', salary: '$ salary', link: 'https://google.com'},
+                {name: 'job 3', company: 'company 3', salary: '$ salary', link: 'https://google.com'},
+                {name: 'job 4', company: 'company 4', salary: '$ salary', link: 'https://google.com'}
             ]
         }
     }
 }
 </script>
+
+<style scoped>
+a {
+    text-decoration: none;
+}
+
+a:link {
+    color: #265301;
+}
+a:visited {
+    color: #551A8B;
+}
+
+.links::before {
+    content: "";
+    display: block;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 1.5em;
+}
+</style>
