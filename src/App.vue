a919cbcb9f5738e01ab34dc2a65c365738a43a38
najib-r <najib72645@gmail.com>
2023-02-16 10:33:44 +0800

    Add placeholder data

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <v-app>
-    <headerContent class="ma-16 pa-16"/>
+    <headerContent class="text-center mx-16 my-5"/>
     <v-main>
+      <mainContent class="mx-16"/>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import headerContent from './components/headerContent.vue';
+import mainContent from './components/mainContent.vue';
 
 export default {
   name: 'App',
 
   components: {
-    headerContent
+    headerContent,
+    mainContent
   },
 
   data: () => ({
--- src/components/mainContent.vue
+++ src/components/mainContent.vue
@@ -1,6 +1,42 @@
 <template>
-
-</template>
+    <v-simple-table dense fixed-header height="300px">
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th>
+              Job position
+            </th>
+            <th>
+              Company
+            </th>
+            <th>
+              Salary
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="job in jobs" :key="job.name">
+            <td>{{ job.name }}</td>
+            <td>{{ job.company }}</td>
+            <td>{{ job.salary }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </template>
 
 <script>
+export default {
+    name: 'mainContent',
+    data () {
+        return {
+            jobs: [
+                {name: 'job 1', company: 'company 1', salary: '$ salary'},
+                {name: 'job 2', company: 'company 2', salary: '$ salary'},
+                {name: 'job 3', company: 'company 3', salary: '$ salary'},
+                {name: 'job 4', company: 'company 4', salary: '$ salary'}
+            ]
+        }
+    }
+}
 </script>
