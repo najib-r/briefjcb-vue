838d2979ebda2710d2251da622ac01113e19ec92
najib-r <najib72645@gmail.com>
2023-02-16 10:12:51 +0800

    Add header

---
+++ src/components/headerContent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p class="text-h5">briefJCB</p>
+        <p class="text-body-1">A simple page showing the 75 most recent job vacancies (top one being most recent) on jobcentrebrunei. Click on a job position to view the job details page on JCB. </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'headerContent'
+}
+</script>
+
+<style scoped>
+</style>
