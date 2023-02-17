43b0aa2e345346410e8c3d2746d76d47b3baf40c
najib-r <najib72645@gmail.com>
2023-02-17 14:22:39 +0800

    Initialise firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD19ZLP9OBbvqH9lYu3y3GcnGGX-Y6UUOE",
+  authDomain: "vue-briefjcb.firebaseapp.com",
+  projectId: "vue-briefjcb",
+  storageBucket: "vue-briefjcb.appspot.com",
+  messagingSenderId: "1031819454266",
+  appId: "1:1031819454266:web:4fe3b4df372199e3161203"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
